9988ee5943801f944fc7b6c2a9c52d0a17e5f3c6
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-05-31 12:18:27 +0530

    Add Python basics code with variable and datatype examples

---
+++ Python/Python_Basics/01_variables_and_datatypes.py
@@ -0,0 +1,32 @@
+integer_variable = 5       # Integer
+float_variable = 3.14    # Float
+string_variable = 'hello' # String
+boolean_variable = True    # Boolean
+
+
+#Printing all the variables 
+
+print(integer_variable)
+print(float_variable)
+print(string_variable)
+print(boolean_variable)
+
+
+#Displaying the type of the variable
+
+print(type(integer_variable))
+print(type(float_variable))
+print(type(string_variable))
+print(type(boolean_variable))
+
+# Converting variable's types to other types
+
+integer_converted_string = str(integer_variable)  # Here the integer_variables is converted into string type of variable
+
+float_converted_integer = int(float_variable)
+
+
+
+
+print(type(integer_converted_string))
+print(type(float_converted_integer))
