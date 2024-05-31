32505673023b5fe59589b2a555e06da4033188ae
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-05-31 12:19:13 +0530

    Add Python set examples

---
+++ Python/Python_Basics/05_set.py
@@ -0,0 +1,18 @@
+my_set = {1, 2, 3, 4}
+
+print(my_set)
+
+# Adding items in the set
+my_set.add(8)
+
+# we cannot add duplicate elements in the set
+my_set.add(8) # This will add 8 only once
+
+print(my_set)
+
+
+# removing element from the set
+my_set.remove(8)
+
+
+print(my_set)
