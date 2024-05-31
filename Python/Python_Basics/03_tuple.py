af3df6eb7ea25f6423790090d46ebf83a8a18f32
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-05-31 12:18:53 +0530

    Add Python tuple examples

---
+++ Python/Python_Basics/03_tuple.py
@@ -0,0 +1,13 @@
+my_tuple  = (1,2,3,4)
+
+print(my_tuple)
+
+# Tuple methods
+print(len(my_tuple))
+print(my_tuple.count(3)) # this will return the count of the element 3 in the tuple
+
+
+# Accessing tuple
+print(my_tuple[1]) # this will print the element 2 as the indexing is 0 based.
+
+# Tuples are unchangable i.e once created we cannot add, update or remove elements in the tuple
