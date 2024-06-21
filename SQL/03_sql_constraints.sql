60e96c7a3974066b2efb947a0bbb5c1e79ea5ff8
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-06-21 16:00:20 +0530

    added other imp constraints

--- SQL/03_sql_constraints.sql
+++ SQL/03_sql_constraints.sql
@@ -31,9 +31,43 @@ primary key(id));
 desc person2;
 
 
+# Foreign Key
+create database company;
+
+use company;
 
 
+create table department(
+department_id int primary key,
+department_name varchar(10)
+);
 
+create table employee(
+emp_id int primary key,
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+department_id int ,
+foreign key(department_id) references department(department_id)
+);
+
+
+desc employee;
+
+
+# check constraint
+
+create table employee1(
+id int primary key,
+first_name varchar(20),
+last_name varchar(20),
+age int,
+check(age >= 18 and age <= 65)
+);
+
+
+insert into employee1(id,first_name,last_name,age) values (
+101, 'Aditya','Kumbhar',20
+);
 
 
 
